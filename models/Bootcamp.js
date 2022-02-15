a9837903e0a07cef799f220bfd842acde9cd4bab
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2022-02-16 00:32:12 +0530

    Add mongoose schema

---
+++ models/Bootcamp.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'Name Required'],
+        unique: [true, 'Name Should be unique'],
+        trim: true,
+        maxlength: [50, 'NAme cannot be be more than 50 Chars']
+    },
+    slug: String,
+    website: {
+        type: String,
+        match: ['https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', 'Regex did not match']
+    },
+    email: {
+        type: String,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please Enter a valid email']
+    },
+    address: {
+        type: String,
+        required: [true, 'Address is required']
+    },
+    location: {
+        type: {
+          type: String,
+          enum: ['Point'], 
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        pincode: String,
+        country: String
+      },
+      careers :{
+          type: [String],
+          required: true,
+          enum: [
+              'Web Development',
+              'Mobile Development',
+              'UI/UX',
+              'Data Science',
+              'Buissness',
+              'Others'
+          ]
+      },
+      rating: {
+          type: Number,
+          min: [1, 'Min length should be 1'],
+          max: [10, 'Max length is 10']
+      },
+      averageCost: Number,
+      photo: {
+          type: String,
+          default: 'no-photo.jpg'
+      },
+      housing: {
+          type: Boolean,
+          default: false
+      },
+      jobAssistance: {
+          type: Boolean,
+          default: false
+      },
+      jobGuarentee: {
+          type: Boolean,
+          default: false
+      },
+      createdAt: {
+          type: Date,
+          default: Date.now()
+      }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
