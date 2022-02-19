b353ab1edd565b63905f4719eda1a9521adee659
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2022-02-20 00:42:35 +0530

    Add initial navbar UI

---
+++ client/oasis-ui/src/landingpage/Landingpage.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import Navbar from "./Navbar";
+import './Landingpage.css';
+
+const Landingpage = () => {
+    return (
+        <div className="top">
+            <Navbar />
+        </div>
+        
+    );
+}
+
+export default Landingpage;
